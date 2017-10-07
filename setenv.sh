2268eae62260a76f503c13e1bfd448ca59b0422d
dennis williams <dennis.williams@estafet.com>
2017-10-07 22:47:30 +0100

    updated env variables.

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,8 @@
 export PROJECT_API_SERVICE_URI=http://localhost:8080/project-api
-export PROJECT_BURNDOWN_SERVICE_URI=http://localhost:8080/project-burndown
 export SPRINT_API_SERVICE_URI=http://localhost:8080/sprint-api
-export SPRINT_BOARD_API_SERVICE_URI=http://localhost:8080/sprint-board-api
 export STORY_API_SERVICE_URI=http://localhost:8080/story-api
+export TASK_API_SERVICE_URI=http://localhost:8080/task-api
+
+export SPRINT_BURNDOWN_SERVICE_URI=http://localhost:8080/sprint-burndown
+export SPRINT_BOARD_API_SERVICE_URI=http://localhost:8080/sprint-board-api
+export PROJECT_API_SERVICE_URI=http://localhost:8080/project-api
