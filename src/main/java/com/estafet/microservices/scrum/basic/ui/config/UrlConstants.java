2c2b0b5a6a1927938e974899b453645dafe252df
shukriev <shukri.izetov.shukriev@gmail.com>
2018-02-27 14:01:52 +0200

    Url Openshift configuration

--- src/main/java/com/estafet/microservices/scrum/basic/ui/config/UrlConstants.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/config/UrlConstants.java
@@ -2,13 +2,16 @@ package com.estafet.microservices.scrum.basic.ui.config;
 
 public class UrlConstants {
 	
-	public static final String STORY_API_SERVICE_URI = System.getenv("STORY_API_SERVICE_URI");
+//	public static final String STORY_API_SERVICE_URI = System.getenv("STORY_API_SERVICE_URI");
+	public static final String STORY_API_SERVICE_URI = "http://gateway.microservices-scrum.svc:8080/story-api";
 //	public static final String STORY_API_SERVICE_URI = "http://localhost:8085/story-api";
-//	public static final String STORY_API_SERVICE_URI = "http://gateway.microservices-scrum.svc:8080/story-api";
+//	public static final String STORY_API_SERVICE_URI = "http://localhost:8089";
 	
 //	public static final String PROJECT_API_SERVICE_URI = System.getenv("PROJECT_API_SERVICE_URI");
 	public static final String PROJECT_API_SERVICE_URI = "http://gateway.microservices-scrum.svc:8080/project-api";
 //	public static final String PROJECT_API_SERVICE_URI = "http://localhost:8085/project-api";
+//	public static final String PROJECT_API_SERVICE_URI = "http://localhost:8086";
+
 	
 //	public static final String SPRINT_API_SERVICE_URI = System.getenv("SPRINT_API_SERVICE_URI");
 	public static final String SPRINT_API_SERVICE_URI = "http://gateway.microservices-scrum.svc:8080/sprint-api";
