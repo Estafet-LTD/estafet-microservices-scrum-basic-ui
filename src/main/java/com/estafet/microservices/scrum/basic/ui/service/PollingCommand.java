f7b9cd85ca11e9e96c9160bb02b37d8cdaf996be
Dennis Williams <dennis.williams@estafet.com>
2018-05-25 23:52:43 +0100

    changed timeout implemenation

--- src/main/java/com/estafet/microservices/scrum/basic/ui/service/PollingCommand.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/service/PollingCommand.java
@@ -1,47 +1,36 @@
 package com.estafet.microservices.scrum.basic.ui.service;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 public abstract class PollingCommand<T> {
 
-	private final Future<T> future;
-	private final ExecutorService executor = Executors.newSingleThreadExecutor();
-	private final Integer timeout;
+	private final int timeout;
+	private static final int DELAY = 500;
 	
 	abstract public boolean isReady(T result);
 	
 	abstract public T result();
 		
 	public PollingCommand() {
-		this(5);
+		this(5000);
 	}
 	
-	public PollingCommand(Integer timeout) {
+	public PollingCommand(int timeout) {
 		this.timeout = timeout;
-		future = executor.submit(new Callable<T>() {
-		    public T call() throws Exception {
-		    	T result = result();
-		    	while (!isReady(result)) {
-					Thread.sleep(500);
-				}
-		        return result;
-		    }
-		});
 	}
 	
 	public T execute() {
 		try {
-			return future.get(timeout, TimeUnit.SECONDS);
-		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			int wait = 0;
+			T result = result();
+			while (!isReady(result)) {
+				Thread.sleep(DELAY);
+				wait+= DELAY;
+				if (wait >= timeout) {
+					throw new RuntimeException("Timeout after " + wait + "ms");
+				}
+			}
+			return result;
+		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
-		} finally {
-			executor.shutdown();
 		}
 	}
 	
