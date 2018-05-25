77ee89921c6930534ae154e5347ac44cf9462b83
Dennis Williams <dennis.williams@estafet.com>
2018-05-25 23:34:46 +0100

    executor shutdown added

--- src/main/java/com/estafet/microservices/scrum/basic/ui/service/PollingCommand.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/service/PollingCommand.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeoutException;
 public abstract class PollingCommand<T> {
 
 	private final Future<T> future;
+	private final ExecutorService executor = Executors.newSingleThreadExecutor();
 	private final Integer timeout;
 	
 	abstract public boolean isReady(T result);
@@ -18,17 +19,16 @@ public abstract class PollingCommand<T> {
 	abstract public T result();
 		
 	public PollingCommand() {
-		this(10000);
+		this(10);
 	}
 	
 	public PollingCommand(Integer timeout) {
 		this.timeout = timeout;
-		ExecutorService executor = Executors.newSingleThreadExecutor();
 		future = executor.submit(new Callable<T>() {
 		    public T call() throws Exception {
 		    	T result = result();
 		    	while (!isReady(result)) {
-					Thread.currentThread().sleep(333);
+					Thread.sleep(333);
 				}
 		        return result;
 		    }
@@ -37,9 +37,11 @@ public abstract class PollingCommand<T> {
 	
 	public T execute() {
 		try {
-			return future.get(timeout, TimeUnit.MILLISECONDS);
+			return future.get(timeout, TimeUnit.SECONDS);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
 			throw new RuntimeException(e);
+		} finally {
+			executor.shutdown();
 		}
 	}
 	
