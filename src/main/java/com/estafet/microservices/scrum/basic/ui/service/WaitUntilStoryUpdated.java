2f5632f65c2f1d87edf6fc1b82df73a4665f5b38
Dennis Williams <dennis.williams@estafet.com>
2018-05-29 23:44:33 +0100

    use wait until from imported jar

--- src/main/java/com/estafet/microservices/scrum/basic/ui/service/WaitUntilStoryUpdated.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/service/WaitUntilStoryUpdated.java
@@ -1,6 +1,7 @@
 package com.estafet.microservices.scrum.basic.ui.service;
 
 import com.estafet.microservices.scrum.basic.ui.model.Story;
+import com.estafet.microservices.scrum.lib.util.WaitUntil;
 
 public class WaitUntilStoryUpdated extends WaitUntil {
 
