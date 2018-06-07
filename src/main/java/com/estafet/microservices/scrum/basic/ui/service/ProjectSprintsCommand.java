e491582fa647f2d8a03bb49869c6baaa31b6f7ba
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-07 11:17:57 +0100

    fixed type conversations for jackson

--- src/main/java/com/estafet/microservices/scrum/basic/ui/service/ProjectSprintsCommand.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/service/ProjectSprintsCommand.java
@@ -1,12 +1,14 @@
 package com.estafet.microservices.scrum.basic.ui.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.client.RestTemplate;
 
 import com.estafet.microservices.scrum.basic.ui.model.Sprint;
-import com.estafet.microservices.scrum.lib.commons.rest.RestHelper;
 import com.estafet.microservices.scrum.lib.commons.wait.WaitUntilCommand;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ProjectSprintsCommand extends WaitUntilCommand<List<Sprint>> {
 
@@ -23,10 +25,19 @@ public class ProjectSprintsCommand extends WaitUntilCommand<List<Sprint>> {
 		return !result.isEmpty();
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	public List<Sprint> result() {
-		return RestHelper.getRestQuery(restTemplate, System.getenv("SPRINT_API_SERVICE_URI") + "/project/{id}/sprints",
-				Sprint.class, projectId);
+		List objects = restTemplate.getForObject(System.getenv("SPRINT_API_SERVICE_URI") + "/project/{id}/sprints",
+				List.class, projectId);
+		List<Sprint> sprints = new ArrayList<Sprint>();
+		ObjectMapper mapper = new ObjectMapper();
+		for (Object object : objects) {
+			Sprint sprint = mapper.convertValue(object, new TypeReference<Sprint>() {
+			});
+			sprints.add(sprint);
+		}
+		return sprints;
 	}
 
 }
