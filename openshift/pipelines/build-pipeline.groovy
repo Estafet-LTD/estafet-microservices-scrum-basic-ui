6a6b4ef0d45444e108039789c2bc2ce7ce5cd17d
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-04 16:28:15 +0100

    pipelines and templates implementation

---
+++ openshift/pipelines/build-pipeline.groovy
@@ -0,0 +1,63 @@
+node("maven") {
+
+	def project = "build"
+	def microservice = "basic-ui"
+
+	currentBuild.description = "Build a container from the source, then execute unit and container integration tests before promoting the container as a release candidate for acceptance testing."
+
+	properties([
+	  parameters([
+	     string(name: 'GITHUB'),
+	  ])
+	])
+
+	stage("checkout") {
+		git branch: "master", url: "https://github.com/${params.GITHUB}/estafet-microservices-scrum-basic-ui"
+	}
+
+	stage("update wiremock") {
+		def files = findFiles(glob: 'src/integration-test/resources/*.json')
+		files.each { file -> 
+			def json = readFile(file.path)
+			def response = httpRequest url: "http://wiremock-docker.${project}.svc:8080/__admin/mappings/new", httpMode: "POST", validResponseCodes: "201", requestBody: json
+		}
+	}
+
+	stage("create build config") {
+			sh "oc process -n ${project} -f openshift/templates/${microservice}-build-config.yml -p NAMESPACE=${project} -p GITHUB=${params.GITHUB} | oc apply -f -"
+	}
+
+	stage("execute build") {
+		openshiftBuild namespace: project, buildConfig: microservice, waitTime: "300000"
+		openshiftVerifyBuild namespace: project, buildConfig: microservice, waitTime: "300000" 
+	}
+
+	stage("create deployment config") {
+		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} | oc apply -f -"
+	}
+
+	stage("execute deployment") {
+		openshiftDeploy namespace: project, depCfg: microservice,  waitTime: "3000000"
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
+	}
+
+	stage("container tests") {
+		withEnv( [ "BASIC_UI_URI=http://${microservice}.${project}.svc:8080" ]) {
+			withMaven(mavenSettingsConfig: 'microservices-scrum') {
+ 				sh "mvn clean verify -P integration-test"
+			} 
+		} 
+	}
+
+	stage("deploy snapshots") {
+		withMaven(mavenSettingsConfig: 'microservices-scrum') {
+ 			sh "mvn clean deploy -Dmaven.test.skip=true"
+		} 
+	}	
+	
+	stage("promote to test") {
+		openshiftTag namespace: project, srcStream: microservice, srcTag: 'latest', destinationNamespace: 'test', destinationStream: microservice, destinationTag: 'PrepareForTesting'
+	}
+
+}
+
---
+++ openshift/pipelines/release-pipeline.groovy
@@ -0,0 +1,76 @@
+def username() {
+    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        return USERNAME
+    }
+}
+
+def password() {
+    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        return PASSWORD
+    }
+}
+
+def project = "test"
+def microservice = "basic-ui"
+
+def developmentVersion
+def releaseVersion
+def releaseTag
+
+node('maven') {
+
+	properties([
+	  parameters([
+	     string(name: 'GITHUB'),
+	  ])
+	])
+
+	stage("checkout") {
+		git branch: "master", url: "https://${username()}:${password()}@github.com/${params.GITHUB}/estafet-microservices-scrum-basic-ui"
+	}
+
+	stage("prepare the database") {
+		withMaven(mavenSettingsConfig: 'microservices-scrum') {
+	      sh "mvn clean package -P prepare-db -Dmaven.test.skip=true -Dproject=${project}"
+	    } 
+	}
+	
+	stage("create deployment config") {
+		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_IMAGE_LABEL=PrepareForTesting | oc apply -f -"
+		sh "oc set env dc/${microservice} JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"
+	}
+	
+	stage("execute deployment") {
+		openshiftDeploy namespace: project, depCfg: microservice,  waitTime: "3000000"
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
+	}
+	
+	stage("increment version") {
+		def pom = readFile('pom.xml');
+		def matcher = new XmlSlurper().parseText(pom).version =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
+		developmentVersion = "${matcher[0][1]}${matcher[0][2].toInteger()+1}-SNAPSHOT"
+		releaseVersion = "${matcher[0][1]}${matcher[0][2]}"
+		releaseTag = "v${releaseVersion}"
+	}
+	
+	stage("perform release") {
+        sh "git config --global user.email \"jenkins@estafet.com\""
+        sh "git config --global user.name \"jenkins\""
+        withMaven(mavenSettingsConfig: 'microservices-scrum') {
+			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -B"
+			sh "git push origin master"
+			sh "git tag ${releaseTag}"
+			sh "git push origin ${releaseTag}"
+		} 
+	}	
+
+	stage("promote image to staging") {
+		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'staging', destinationStream: microservice, destinationTag: releaseVersion
+	}	
+	
+	stage("trigger acceptance tests") {
+		sh "oc start-build qa-pipeline -n cicd"	
+	}
+	
+}
+
