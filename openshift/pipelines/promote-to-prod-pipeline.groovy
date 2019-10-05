669a7db8ad07fe1effa33ad6e20b4fd5ec630451
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-05 21:20:41 +0100

    implementation of the promote to production pipeline

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -1,85 +1,65 @@
 @NonCPS
-def getImageStreams(is) {
-	def count = 0
-	def imageStreams = []
-	is.split('\n').each { line ->
-    if (count > 0) {
-        def matcher = line =~ /(\w+\-\w+)(.*)/
-        imageStreams << matcher[0][1]
-    }
-    count++
-	}
-	return imageStreams
-}
-
-@NonCPS
-def getImage(json) {
-	def imageId = new groovy.json.JsonSlurper().parseText(json).status.containerStatuses[0].imageID
-	def matcher = imageId =~ /(.*\@sha256\:)(\w+)/
-	return matcher[0][2]
-}
-
-@NonCPS
-def getLatest(json) {
+def getVersions(json) {
 	def tags = new groovy.json.JsonSlurper().parseText(json).status.tags
+	def versions = []
 	for (int i = 0; i < tags.size(); i++) {
-		if (tags[i]['tag'].equals("latest")) {
-			def image = tags[i]['items'][0]['image']
-			def matcher = image =~ /(sha256\:)(\w+)/
-			return matcher[0][2]
-		}
+		versions << tags[i]['tag']
 	}
-	return null
+	return versions
 }
 
-def isLatestImageDeployed(microservice) {
-	def pod = getPod microservice
-	if (pod == null) return false
-	def podImage = getPodImage pod
-	def latestImage = getLatestImage microservice
-	return podImage.equals(latestImage)
+@NonCPS
+def getNamespace(json) {
+	def matcher = new groovy.json.JsonSlurper().parseText(json).items[0].spec.to.name =~ /(green|blue)(\-basic\-ui)/
+	String namespace = matcher[0][1]
+	return namespace.equals("green") ? "blue" : "green" 
 }
 
-
-def username() {
-    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        return USERNAME
-    }
+def recentVersion( versions ) {
+	def size = versions.size()
+	return versions[size-1]
 }
 
-def password() {
-    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        return PASSWORD
-    }
+def getLatestVersion(microservice) {
+	sh "oc get is ${microservice} -o json -n staging > image.json"
+	def image = readFile('image.json')
+	def versions = getVersions(image)
+	if (versions.size() == 0) {
+		throw new RuntimeException("There are no images for ${microservice}")
+	}
+	return recentVersion(versions)
 }
 
-
-def microservice = "basic-ui"
-
-
-node('maven') {
+node {
+	
+	def project
+	def version
+	def microservice = "basic-ui"
 
 	properties([
 	  parameters([
 	     string(name: 'GITHUB'),
 	  ])
 	])
-
-	stage("checkout") {
-		git branch: "master", url: "https://github.com/${params.GITHUB}/estafet-microservices-scrum-basic-ui"
+	
+	stage("determine the environment to deploy to") {
+		sh "oc get route -o json -n live > route.json"
+		def route = readFile('route.json')
+		project = getNamespace(route)
+		println "the target namespace is $project"
 	}
 	
-	// derive the environment to deploy to
-	def project = "test"
+	stage("determine which image is to be deployed") {
+		version = getLatestVersion microservice
+		println "latest version is $version"
+	}
 	
-	// perform health check to check if deploed
-	
-	// get the image and check if referenced
-	
-	// deploy
+	stage("checkout release version") {
+		git tag: version, url: "https://github.com/${params.GITHUB}/estafet-microservices-scrum-basic-ui"
+	}
 	
 	stage("create deployment config") {
-		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_IMAGE_LABEL=PrepareForTesting | oc apply -f -"
+		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p DOCKER_NAMESPACE=staging -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
 		sh "oc set env dc/${microservice} SPRINT_API_SERVICE_URI=http://sprint-api.${project}.svc:8080 STORY_API_SERVICE_URI=http://story-api.${project}.svc:8080 TASK_API_SERVICE_URI=http://task-api.${project}.svc:8080 PROJECT_API_SERVICE_URI=http://project-api.${project}.svc:8080 SPRINT_BOARD_API_SERVICE_URI=http://sprint-board.${project}.svc:8080 SPRINT_BURNDOWN_SERVICE_URI=http://sprint-burndown.${project}.svc:8080 PROJECT_BURNDOWN_SERVICE_URI=http://project-burndown.${project}.svc:8080 JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"
 	}
 	
@@ -87,7 +67,6 @@ node('maven') {
 		openshiftDeploy namespace: project, depCfg: microservice,  waitTime: "3000000"
 		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
 	}
-
 
 }
 
