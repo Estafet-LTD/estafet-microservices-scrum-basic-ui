4a1bbaa6d27736e172df1f66b3d31cb0eaff5487
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-06 18:29:06 +0100

    added code to check for non existent pods

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -55,12 +55,19 @@ boolean isLatestVersionDeployed(project, microservice, version) {
 	def image = readFile('image.json')
 	def imageStreamHash = getImageStreamHash(image, version)
 	println "image stream hash $imageStreamHash"
-	sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} -o json > pod.json"
-	def pod = readFile('pod.json')
-	def podImageHash = getPodImageHash(pod)
-	println "pod image hash $podImageHash"
-	return imageStreamHash.equals(podImageHash)
+	sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} > exists.json"
+	def exists =  = readFile('exists.json')
+	if (!exists.indexOf("No resources")>= 0) {
+		sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} -o json > pod.json"
+		def pod = readFile('pod.json')
+		def podImageHash = getPodImageHash(pod)
+		println "pod image hash $podImageHash"
+		return imageStreamHash.equals(podImageHash)	
+	} else {
+		return false
+	}
 }
+
 
 node {
 	
