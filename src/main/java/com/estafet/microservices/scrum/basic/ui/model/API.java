f1574a162b6aa63f5f57ace79f10baa6b32d2903
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-06 01:05:27 +0100

    hack out the version

--- src/main/java/com/estafet/microservices/scrum/basic/ui/model/API.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/model/API.java
@@ -1,11 +1,17 @@
 package com.estafet.microservices.scrum.basic.ui.model;
 
+import java.util.StringTokenizer;
+
 public class API {
 
 	private String version;
 
 	public API(String version) {
-		this.version = version;
+		StringTokenizer tokenizer = new StringTokenizer(version.replaceAll("\\-SNAPSHOT", ""), ".");
+		String p1 = tokenizer.nextToken();
+		String p2 = tokenizer.nextToken();
+		String p3 = tokenizer.nextToken();
+		this.version = p1 + "." + p2 + "." + Integer.toString(Integer.parseInt(p3) - 1);
 	}
 
 	public String getVersion() {
