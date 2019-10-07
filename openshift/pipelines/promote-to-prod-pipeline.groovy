b8fd4812488cb4dbcd63022deb5558b8150a1368
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-07 22:26:15 +0100

    more changes

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -55,10 +55,10 @@ boolean isLatestVersionDeployed(project, microservice, version) {
 	def image = readFile('image.json')
 	def imageStreamHash = getImageStreamHash(image, version)
 	println "image stream hash $imageStreamHash"
-	sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} > exists.json"
+	sh "oc get pods --selector deploymentconfig=${env}${microservice} -n ${project} > exists.json"
 	def exists = readFile('exists.json')
 	if (exists.indexOf("No resources")>= 0) {
-		sh "oc get pods --selector deploymentconfig=${microservice} -n ${project} -o json > pod.json"
+		sh "oc get pods --selector deploymentconfig=${env}${microservice} -n ${project} -o json > pod.json"
 		def pod = readFile('pod.json')
 		def podImageHash = getPodImageHash(pod)
 		println "pod image hash $podImageHash"
