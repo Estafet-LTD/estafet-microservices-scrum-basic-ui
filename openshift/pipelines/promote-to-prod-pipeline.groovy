81fd5023efd1412cac99c5c63d6ca5fac2a78a87
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-10-07 20:50:15 +0100

    use the full name for production

--- openshift/pipelines/promote-to-prod-pipeline.groovy
+++ openshift/pipelines/promote-to-prod-pipeline.groovy
@@ -102,7 +102,7 @@ node {
 	
 	stage("create deployment config") {
 		sh "oc process -n ${project} -f openshift/templates/${microservice}-config.yml -p NAMESPACE=${project} -p ENV=${env} -p DOCKER_NAMESPACE=${project} -p DOCKER_IMAGE_LABEL=${version} | oc apply -f -"
-		sh "oc set env dc/${microservice} SPRINT_API_SERVICE_URI=http://${env}sprint-api.${project}.svc:8080 STORY_API_SERVICE_URI=http://${env}story-api.${project}.svc:8080 TASK_API_SERVICE_URI=http://${env}task-api.${project}.svc:8080 PROJECT_API_SERVICE_URI=http://${env}project-api.${project}.svc:8080 SPRINT_BOARD_API_SERVICE_URI=http://${env}sprint-board.${project}.svc:8080 SPRINT_BURNDOWN_SERVICE_URI=http://${env}sprint-burndown.${project}.svc:8080 PROJECT_BURNDOWN_SERVICE_URI=http://${env}project-burndown.${project}.svc:8080 JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
+		sh "oc set env dc/${env}${microservice} SPRINT_API_SERVICE_URI=http://${env}sprint-api.${project}.svc:8080 STORY_API_SERVICE_URI=http://${env}story-api.${project}.svc:8080 TASK_API_SERVICE_URI=http://${env}task-api.${project}.svc:8080 PROJECT_API_SERVICE_URI=http://${env}project-api.${project}.svc:8080 SPRINT_BOARD_API_SERVICE_URI=http://${env}sprint-board.${project}.svc:8080 SPRINT_BURNDOWN_SERVICE_URI=http://${env}sprint-burndown.${project}.svc:8080 PROJECT_BURNDOWN_SERVICE_URI=http://${env}project-burndown.${project}.svc:8080 JAEGER_AGENT_HOST=jaeger-agent.${project}.svc JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 JAEGER_SAMPLER_PARAM=1 JAEGER_SAMPLER_TYPE=const -n ${project}"	
 	}
 	
 	stage("execute deployment") {
